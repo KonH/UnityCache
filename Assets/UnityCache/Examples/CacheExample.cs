cedd39c5acf365374ba5f29913175ca4228ca0c0
KonH <konh@yandex.ru>
2016-06-18 10:32:17 +0600

    Fixed formatting, use FixedUpdate instead of Update, reset PreCached example (need to use UnityCache/PreCache first);

--- Assets/UnityCache/Examples/CacheExample.cs
+++ Assets/UnityCache/Examples/CacheExample.cs
@@ -3,27 +3,27 @@ using UnityEngine;
 using UnityCache;
 
 public class CacheExample : MonoBehaviour {
-    // This members would be initialized on load
-    // Only public members
-    [Cached]
-    public Transform MyTransform = null;
-    // But you can cache hide-in-inspector objects
-    [HideInInspector]
-    [Cached] 
-    public Rigidbody MyRigidbody = null;
+	// This members would be initialized on load
+	// Only public members
+	[Cached]
+	public Transform MyTransform = null;
+	// But you can cache hide-in-inspector objects
+	[HideInInspector]
+	[Cached]
+	public Rigidbody MyRigidbody = null;
 
-    void Awake() {
-        // Cache all [Cached] members
-        Cache.CacheAll(this);
-    }
+	void Awake() {
+		// Cache all [Cached] members
+		Cache.CacheAll(this);
+	}
 
 	void Start () {
-        // Now we can use cached items
-        var pos = MyTransform.position;
+		// Now we can use cached items
+		var pos = MyTransform.position;
 	}
 	
-	void Update () {
-        // Even hidden one
-        MyRigidbody.AddForce(Vector3.forward * 10 * Time.deltaTime);
+	void FixedUpdate () {
+		// Even hidden one
+		MyRigidbody.AddForce(Vector3.forward * 10 * Time.fixedDeltaTime);
 	}
 }
--- Assets/UnityCache/Examples/PreCacheExample.cs
+++ Assets/UnityCache/Examples/PreCacheExample.cs
@@ -3,26 +3,26 @@ using UnityEngine;
 using UnityCache;
 
 public class PreCacheExample : MonoBehaviour {
-    // This members would be cached in scene objects after execute UnityCache/PreCache command in menu
-    // Only public members
-    [PreCached]
-    public Transform MyTransform = null;
-    // But you can cache hide-in-inspector objects
-    [HideInInspector]
-    [PreCached] 
-    public Rigidbody MyRigidbody = null;
+	// This members would be cached in scene objects after execute UnityCache/PreCache command in menu
+	// Only public members
+	[PreCached]
+	public Transform MyTransform = null;
+	// But you can cache hide-in-inspector objects
+	[HideInInspector]
+	[PreCached] 
+	public Rigidbody MyRigidbody = null;
 
-    void Awake() {
-        // No field initialization on load!
-    }
+	void Awake() {
+		// No field initialization on load!
+	}
 
 	void Start () {
-        // Now we can use cached items
-        var pos = MyTransform.position;
+		// Now we can use cached items
+		var pos = MyTransform.position;
 	}
 	
-	void Update () {
-        // Even hidden one
-        MyRigidbody.AddForce(Vector3.forward * 10 * Time.deltaTime);
+	void FixedUpdate () {
+		// Even hidden one
+		MyRigidbody.AddForce(Vector3.forward * 10 * Time.fixedDeltaTime);
 	}
 }
